dcdc62da64d81527e1fc7cbbbec501d550d0bff2
niejia <niefuling@yonglibao.com>
2017-10-18 23:57:37 +0800

    后台模板

---
+++ 后台模板/扁平化的人格障碍诊断系统管理后台模板/datas/nav.js
@@ -0,0 +1,62 @@
+var navs = [{
+	
+	"title": "首页",
+	"icon": "fa-cubes",
+	"spread": true,
+	"href": "main.html"
+}, {
+	"title": "管理",
+	"icon": "fa-cogs",
+	"spread": false,
+	"children": [{
+		"title": "档案管理",
+		"icon": "fa-table",
+		"href": "danganguanli.html"
+	}, {
+		"title": "数据档案",
+		"icon": "fa-table",
+		"href": "shujudangan.html"
+	}, {
+		"title": "用户档案",
+		"icon": "fa-navicon",
+		"href": "icheck.html"
+	}]
+}, {
+	"title": "设置",
+	"icon": "fa-address-book",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "用户权限",
+		"icon": "&#xe63c",
+		"href": "begtable.html"
+	}, {
+		"title": "数据备份",
+		"icon": "&#xe63c",
+		"href": "btable.html"
+	}, {
+		"title": "登录权限",
+		"icon": "&#xe63c;",
+		"href": "btable.html"
+	}]
+}, {
+	"title": "远程分析",
+	"icon": "fa-stop-circle",
+	"href": "table.html",
+	"spread": false
+}, {
+	"title": "诊断报告",
+	"icon": "fa-stop-circle",
+	"href": "main.html",
+	"spread": false,
+	
+},
+{
+	"title": "自动诊断",
+	"icon": "fa-stop-circle",
+	"href": "auxiliar.html",
+	"spread": false,
+	
+}
+];
+
